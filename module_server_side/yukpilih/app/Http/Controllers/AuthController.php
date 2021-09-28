f5118fc410ff746073e7298f3e2405958beb135d
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-09-28 11:00:06 +0700

    Add AuthController, Authentication

--- module_server_side/yukpilih/app/Http/Controllers/AuthController.php
+++ module_server_side/yukpilih/app/Http/Controllers/AuthController.php
@@ -2,16 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use JWTAuth;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        if (Auth::attempt(['username' => $request->email, 'password' => $request->password])) {
+        $validator = Validator::make($request->only('username', 'password'), [
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ], 400);
         }
+
+        if ($token = JWTAuth::attempt($request->only('username', 'password'))) {
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'expires_in' => '24 Hourse'
+            ]);
+        }
+        return response()->json([
+            'error' => 'Unauthorized'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $validator = Validator::make($request->only('token'), [
+            'token' => ['required'],
+        ]);
+
+        if ($validator->fails()) return response()->json([
+            'message' => 'Unauthorized',
+        ], 401);
+
+        JWTAuth::invalidate($request->token);
+
+        return response()->json([
+            'message' => 'successfully logged out'
+        ]);
     }
 }
--- module_server_side/yukpilih/routes/api.php
+++ module_server_side/yukpilih/routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
