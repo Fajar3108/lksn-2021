2a6875c32576192bc18deaaff270c1e25bf4fda6
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-09-28 10:20:28 +0700

    Add Migration

---
+++ module_server_side/yukpilih/database/migrations/2021_09_28_030646_create_polls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('created_by')->constrained('users');
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('deadline');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polls');
+    }
+}
